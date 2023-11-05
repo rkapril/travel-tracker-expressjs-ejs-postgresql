e3deebfdae9216a04e1896aae3bb7edd482a7635
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-11-06 00:23:47 +0800

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -1,22 +1,3 @@
-CREATE TABLE IF NOT EXISTS captials (
-	id SERIAL PRIMARY KEY,
-	country VARCHAR(45),
-	captial VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS flags (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(45),
-	flag TEXT
-);
-
-CREATE TABLE IF NOT EXISTS world_food (
-	id SERIAL PRIMARY KEY,
-	country VARCHAR(45),
-	rice_production FLOAT,
-	wheat_production FLOAT
-);
-
 CREATE TABLE IF NOT EXISTS visited_countries (
 	id SERIAL PRIMARY KEY,
 	country_code CHAR(2) NOT NULL UNIQUE
